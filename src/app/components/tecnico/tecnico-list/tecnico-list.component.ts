8fa71b3f5906921b984411767508f80868238816
Robson Lopes <rob_lopes7@hotmail.com>
2022-01-11 08:36:39 -0300

    Complemento de Tecnico list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -2,6 +2,7 @@ import { Tecnico } from './../../../models/tecnico';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -10,33 +11,30 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class TecnicoListComponent implements OnInit {
 
-  ELEMENT_DATA: Tecnico[] = [
-    {
-      id: 1,
-      nome: 'Robson Lopes',
-      cpf:'0000000000-00',
-      email: 'teste@gmail.com',
-      senha: '1234',
-      perfis:[
-        '0'
-      ],
-      dataCriacao:'10/01/2022'
-    }
-  ];
+  ELEMENT_DATA: Tecnico[] = [];
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  constructor(private tecnicoService:TecnicoService) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(){
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
---
+++ src/app/services/tecnico.service.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+import { API_CONFIG } from './../config/api.config';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Tecnico } from '../models/tecnico';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TecnicoService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  findAll(): Observable<Tecnico[]> {
+    return this.httpClient.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
+  }
+
+}
